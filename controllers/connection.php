8d399725e1dbe03c613ede8d3ed131dc9f922bd4
dmnccruz <70881863+dmnccruz@users.noreply.github.com>
2020-09-12 12:06:46 +0800

    Update connection.php

--- controllers/connection.php
+++ controllers/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-    $host = "localhost";
-    $db_username = "root"; 
-    $db_password = ""; 
-    $db_name = "b67todolist"; 
+    $host = "z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $db_username = "rz2msuye672wrzir"; 
+    $db_password = "bwv9h0f7c9j31q5r"; 
+    $db_name = "h6878riv4kuf6wwm"; 
 
     $conn = mysqli_connect($host, $db_username, $db_password, $db_name);
 
