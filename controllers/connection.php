4bd6ea11d1156ebbc394d183559bca8b0f3f840c
dominicmartincruz <dominicmartincruz@gmail.com>
2020-09-12 21:22:31 +0800

    added website icon

--- controllers/connection.php
+++ controllers/connection.php
@@ -1,17 +1,17 @@
 <?php
 
-    $host = "localhost";
-    $db_username = "root"; 
-    $db_password = ""; 
-    $db_name = "b67todolist"; 
+    $host = "z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $db_username = "rz2msuye672wrzir"; 
+    $db_password = "bwv9h0f7c9j31q5r"; 
+    $db_name = "h6878riv4kuf6wwm"; 
 
     $conn = mysqli_connect($host, $db_username, $db_password, $db_name);
 
-    // if(!$conn){
-    //     die("Connection failed: " . mysqli_error($conn));
-    // }
-    // else {
-    //     die("Connected.");
+//     if(!$conn){
+//         die("Connection failed: " . mysqli_error($conn));
+//     }
+//     else {
+//         die("Connected.");
 
-    // }
+//     }
 ?>
